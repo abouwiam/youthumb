2dce2c09768d58546bbfb46ea516fd0d443dae0c
abouwiam <138531717+abouwiam@users.noreply.github.com>
2023-09-03 15:29:30 +0100

    adding change

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <DefaultSeo
-        title="Free Thumbnail Downloader Youtube"
+        title="Free Thumbnail Downloader Youtube - High-Quality and 4k Thumbnail Images"
         description="Discover thumbnail downloader YouTube tool: Access high-quality thumbnails in various resolutions and discover inspiration from fellow creators' designs. Simply paste your video URL, hit 'Download Thumbnails,' and instantly enhance your content with eye-catching visuals. Start optimizing your YouTube presence today"
         canonical="https://thumbnaildownloaderyoutube.com"
         openGraph={{
