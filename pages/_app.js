0498a84549ebdeaac6111e7feb22c559b7730139
abouwiam <138531717+abouwiam@users.noreply.github.com>
2023-09-03 14:13:27 +0100

    adding change

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <DefaultSeo
-        title="Thumbnail Downloader Youtube"
-        description="Download high-quality and 4k thumbnails from YouTube videos Online."
+        title="Free Thumbnail Downloader Youtube"
+        description="Discover Thumbnail Downloader YouTube – the simple solution for grabbing top-notch YouTube video thumbnails at no cost. With our easy-to-use tool, you can download thumbnails in various qualities. Just paste the video thumbnail URL in the box below, click, and Get a Thumbnail Image. Elevate your content with stunning visuals today!"
         canonical="https://thumbnaildownloaderyoutube.com"
         openGraph={{
           url: "https://thumbnaildownloaderyoutube.com",
-          title: "Thumbnail Downloader Youtube",
-          description: "Download high-quality and 4k thumbnails from YouTube videos Online.",
-          site_name: "Thumbnail Downloader Youtube",
+          title: "Free Thumbnail Downloader Youtube",
+          description: "Discover Thumbnail Downloader YouTube – the simple solution for grabbing top-notch YouTube video thumbnails at no cost. With our easy-to-use tool, you can download thumbnails in various qualities. Just paste the video thumbnail URL in the box below, click, and Get a Thumbnail Image. Elevate your content with stunning visuals today!",
+          site_name: "Free Thumbnail Downloader Youtube",
         }}
       />
       <Component {...pageProps} />
